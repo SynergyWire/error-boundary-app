3b3b80675ffdaf40fddaf5f617ebf4ade30cd130
Luis Santiago <luis.santiago@itglobers.com>
2023-08-03 11:58:22 -0500

    feat: ErrorBoundary implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { ErrorBoundary, useErrorBoundary } from 'react-error-boundary'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ProductData {
+  productName: string
+  productReference: string
+  price: number
 }
 
-export default App;
+const products = {
+  '1': {
+    productName: 'xxxx',
+    price: 0
+  },
+  '2': {
+    productName: 'xxxx',
+    price: 0
+  }
+} as unknown as Record<string, ProductData>
+
+function makeRequestClosure() {
+  let count = 0
+
+  return async function (productId: string): Promise<ProductData> {
+    const product = products[productId]
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (product) {
+          
+          if (count > 0) {
+            product.productReference = `${productId}-xxxx`
+          }
+
+          resolve(product)
+          count++
+        } else {
+          reject(new Error('It was not product found'))
+        }
+
+      }, 500)
+    })
+  }
+}
+
+const makeRequest = makeRequestClosure()
+
+const productDetails: Record<string, Record<string, string>> = {
+  '1-xxxx': {
+    care: 'Do not wash in washing machine'
+  },
+  '2-xxxx': {
+    care: 'Do not wash in washing machine'
+  }
+}
+
+function ProductInfo ({ product }: {product: ProductData}) {
+
+  return (
+    <div>
+      <p>Name: {product.productName}</p>
+      <p>Reference: {product.productReference}</p>
+      <p>Price: {product.price}</p>
+      <p>Care: {productDetails[product.productReference].care}</p>
+    </div>
+  )
+}
+
+interface ErrorFallbackProps {
+  error: Error
+}
+
+function ErrorFallback ({ error }: ErrorFallbackProps) {
+  const { resetBoundary } = useErrorBoundary()
+
+  return (
+    <div>
+      <p>There was an error: <strong>{error.message}</strong></p>
+      <button onClick={resetBoundary}>Try Again</button>
+    </div>
+  )
+}
+
+  function App() {
+    const [productData, setProductData] = React.useState<ProductData | null>(null)
+    const [error, setError] = useState<string | null>(null)
+    const [status, setStatus] = useState('idle')
+    const inputRef = React.useRef<HTMLInputElement>(null)
+
+    const handleSearch = () => {
+      getProductData()
+    }
+
+    const getProductData = () => {
+      const productId = inputRef.current?.value ?? ''
+
+      if (!productId) {
+        return
+      }
+
+      setStatus('pending')
+      makeRequest(productId)
+        .then((data) => {
+          setStatus('resolved')
+          setProductData(data)
+        })
+        .catch((error: Error) => {
+          setStatus('rejected')
+          setError(error.message)
+        })
+    }
+
+    const renderContent = () => {
+      switch (status) {
+        case 'pending':
+          return (
+            <p>Loading</p>
+          )
+        case 'resolved':
+          return (
+            <ProductInfo product={productData!} />
+          )
+        case 'rejected':
+          return (
+            <div>{error}</div>
+          )
+        default:
+          return (
+            <p>Type a product ID</p>
+          )
+      }
+    }
+
+    return (
+      <div>
+        <div>
+          <input type="text" ref={inputRef} />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        <div>
+          <ErrorBoundary 
+            FallbackComponent={ErrorFallback} 
+            onReset={getProductData}
+            resetKeys={[status]}
+          >
+            {renderContent()}
+          </ErrorBoundary>
+        </div>
+      </div>
+    )
+  } 
+
+export default App
